242ad110e130f5f2fe63f597b9df4b788257a508
jasonnguyen11141 <nguy1442@msu.edu>
2025-02-23 23:06:22 +0000

    finished code

--- utilities.js
+++ utilities.js
@@ -1,4 +1,8 @@
-function random(){
-    Math.floor(Math.random() *10)
-}
 
+function random(){
+    const names = ['Jason', 'Aaron', 'Charlie', 'Lola', 'John', 'Ida', 'Kamran', 'Eddie', 'Lila', 'Romeo'];
+    const number = Math.floor(Math.random() * 11);
+    const name = names[number];
+
+    alert(name)
+}
